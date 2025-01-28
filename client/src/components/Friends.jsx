b9bcbd3b0afc3a2aa0daecc7cab94afaa4b9d431
Your Name <avondet2005@gmail.com>
2025-01-28 12:45:38 -0500

    Friends switch button

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../utilities.css";
 import "./Friends.css";
+import "./Switch.css";
 
-import {get, post} from '../utilities';
+import { get, post } from "../utilities";
 
-import {UserContext} from "./context/UserContext";
+import { UserContext } from "./context/UserContext";
 
 const Friends = () => {
+  const [active, setActive] = useState("left"); // "left" is initially active
 
+  const toggleActive = (side) => {
+    setActive(side); // Update the active button
+  };
 
-    return (
-        <div className="friends-container">
-            <h3>Friends</h3>
-
-        </div>
-    );
-
+  return (
+    <div className="friends-container">
+      <div className="switch-container">
+        <button
+          className={`switch-button ${active === "left" ? "active" : ""}`}
+          onClick={() => toggleActive("left")}
+        >
+          Friends
+        </button>
+        <button
+          className={`switch-button ${active === "right" ? "active" : ""}`}
+          onClick={() => toggleActive("right")}
+        >
+          Requests
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Friends;
